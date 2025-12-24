04224ead508d16421eacb43774e459ced81494e5
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-12-24 20:23:38 +0530

    Configure Aceterity Ui library and buid the navbar and add dummy json music data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-   <h1 className="text-2xl text-center">Hello World</h1>
+    <main className="min-h-screen bg-black/[0/96 antialiased bg-grid-white/[0.2]">
+      <h1 className="text-2xl text-center">Hello World</h1>
+    </main>
   );
 }
