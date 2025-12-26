25e4d56f15e48cd2949bb665d833eb8c95f4b1bb
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-12-26 19:23:31 +0530

    Successfully comppleted Card seccion ui and display the card dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FaeturedCourses from "@/components/FaeturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0/96 antialiased bg-grid-white/[0.2]">
       <HeroSection/>
+      <FaeturedCourses/>
     </main>
   );
 }
